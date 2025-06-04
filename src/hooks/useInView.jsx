0ca7b47076611a9867577aeda8ce0de9c99b7b62
Vivian Tang <viviantang.dev@gmail.com>
2025-06-04 13:01:20 +0200

    hook(useInView): Created hook

---
+++ src/hooks/useInView.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useInView = (options = {}) => {
+  const ref = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+        }
+      },
+      { threshold: 0.2, ...options }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, [options]);
+
+  return [ref, isInView];
+};
